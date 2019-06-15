6ac60d0945867d8d1546c7ed408d4e7096d496b2
Brice <brice.augustin@u-pec.fr>
2019-06-16 00:21:30 +0200

    Create 3 test VMs for VirtualBox

--- test/build.sh
+++ test/build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+vboxmanage unregistervm rh2 --delete
+
+vboxmanage unregistervm rh3 --delete
+
+vboxmanage snapshot rh delete rhsnap
+
+vboxmanage unregistervm rh --delete
+
 rm -rf output-virtualbox-ovf/
 
-packer build test/rh.json
+packer build rh.json
+
+vboxmanage snapshot rh take rhsnap
+
+vboxmanage clonevm rh --snapshot rhsnap --name rh2 --options link --register
+
+vboxmanage clonevm rh --snapshot rhsnap --name rh3 --options link --register
